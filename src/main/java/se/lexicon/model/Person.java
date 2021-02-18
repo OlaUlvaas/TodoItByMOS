ffdb66ed08a919bbc28bc4bf3d538111b89b20cb
olaulvas <ola.ulvaas@gmail.com>
2021-02-18 15:14:52 +0100

    new Branch, db-connection, renamed fields in Person and Todo

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -1,25 +1,73 @@
 package se.lexicon.model;
 import se.lexicon.data.PersonSequencer;
+
+import java.util.Objects;
+
 public class Person {
-    private final int PERSONID;
-    private String firstName;
-    private String lastName;
+
+
+    private int person_id;
+    private String first_Name;
+    private String last_Name;
+
     public Person() {
-        PERSONID = PersonSequencer.nextPersonId();
+
     }
-    public int getPERSONID() {
-        return PERSONID;
+
+    public Person(String first_Name, String last_Name) {
+        this.first_Name = first_Name;
+        this.last_Name = last_Name;
     }
-    public String getFirstName() {
-        return firstName;
+
+    public Person(int person_id, String first_Name, String last_Name) {
+        this.person_id = person_id;
+        this.first_Name = first_Name;
+        this.last_Name = last_Name;
     }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+
+    public int getPerson_id() {
+        return person_id;
     }
-    public String getLastName() {
-        return lastName;
+
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
     }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+
+    public String getFirst_Name() {
+        return first_Name;
+    }
+
+    public void setFirst_Name(String first_Name) {
+        this.first_Name = first_Name;
+    }
+
+    public String getLast_Name() {
+        return last_Name;
+    }
+
+    public void setLast_Name(String last_Name) {
+        this.last_Name = last_Name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return person_id == person.person_id && Objects.equals(first_Name, person.first_Name) && Objects.equals(last_Name, person.last_Name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(person_id, first_Name, last_Name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "person_id=" + person_id +
+                ", first_Name='" + first_Name + '\'' +
+                ", last_Name='" + last_Name + '\'' +
+                '}';
     }
 }
--- src/main/java/se/lexicon/model/Todo.java
+++ src/main/java/se/lexicon/model/Todo.java
@@ -1,39 +1,122 @@
 package se.lexicon.model;
+import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Scanner;
 import se.lexicon.data.TodoSequencer;
 public class Todo {
-    private final int TODOID;
+
+    private int todo_id;
+    private String title;
     private String description;
+    private LocalDate deadline;
     private boolean done;
-    private Person assignee;
+    private int assignee_id;
 
     public Todo() {
-        TODOID = TodoSequencer.nextTodoId();
+
     }
-    public Todo(String description) {
+    public Todo(String title, String description, LocalDate deadline, boolean done) {
+        this.title = title;
         this.description = description;
-        TODOID = TodoSequencer.nextTodoId();
+        this.deadline = deadline;
+        this.done = done;
+        this.assignee_id = assignee_id;
     }
-    public int getTODOID() {
-        return TODOID;
+    public Todo(String title, String description, LocalDate deadline, boolean done, int assignee_id) {
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.done = done;
+        this.assignee_id = assignee_id;
     }
+
+    public Todo(int todo_id, String title, String description, LocalDate deadline, boolean done) {
+        this.todo_id = todo_id;
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.done = done;
+        this.assignee_id = assignee_id;
+    }
+    public Todo(int todo_id, String title, String description, LocalDate deadline, boolean done, int assignee_id) {
+        this.todo_id = todo_id;
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.done = done;
+        this.assignee_id = assignee_id;
+    }
+
+    public int getTodo_id() {
+        return todo_id;
+    }
+
+    public void setTodo_id(int todo_id) {
+        this.todo_id = todo_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
     public boolean isDone() {
         return done;
     }
+
     public void setDone(boolean done) {
         this.done = done;
     }
-    public Person getAssignee() {
 
-        return assignee;
+    public int getAssignee_id() {
+        return assignee_id;
     }
-    public void setAssignee(Person assignee) {
-        this.assignee = assignee;
+
+    public void setAssignee_id(int assignee_id) {
+        this.assignee_id = assignee_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return todo_id == todo.todo_id && done == todo.done && assignee_id == todo.assignee_id && Objects.equals(title, todo.title) && Objects.equals(description, todo.description) && Objects.equals(deadline, todo.deadline);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(todo_id, title, description, deadline, done, assignee_id);
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "todo_id=" + todo_id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", deadline=" + deadline +
+                ", done=" + done +
+                ", assignee_id=" + assignee_id +
+                '}';
     }
 }
